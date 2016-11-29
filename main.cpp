cbfeaf1fe7d360558e053ee17cbf7a5ad20a21e0
shramru <shramru@gmail.com>
2016-11-29 21:22:08 +0300

    refactor

--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
 #include <opencv2/opencv.hpp>
 #include <dirent.h>
 
-cv::Point top, bottom;
-cv::Mat img, drawn;
-bool down = false;
+static const int S = 536871027;
+static const int SPACE = 536870944;
 
-void CallBackFunc(int event, int x, int y, int flags, void* userdata) {
-    if (event == cv::EVENT_LBUTTONDOWN) {
-        top = cv::Point(x, y);
-        down = true;
-    } else if (down && event == cv::EVENT_MOUSEMOVE) {
-        bottom = cv::Point(x, y);
-        cv::rectangle(drawn, top, bottom, cv::Scalar(255, 0, 0), 3);
-        cv::imshow("My Window", drawn);
-        drawn = img.clone();
-    } else if (event == cv::EVENT_LBUTTONUP) {
-        down = false;
+static cv::Point top, bottom;
+static cv::Mat img, drawn;
+static bool down = false;
+
+void callback(int event, int x, int y, int, void*) {
+    switch (event) {
+        case cv::EVENT_LBUTTONDOWN:
+            down = true;
+            top = cv::Point(x, y);
+            break;
+        case cv::EVENT_LBUTTONUP:
+            down = false;
+            break;
+        case cv::EVENT_MOUSEMOVE:
+            if (down) {
+                bottom = cv::Point(x, y);
+                cv::rectangle(drawn, top, bottom, cv::Scalar(255, 0, 0), 3);
+                cv::imshow("My Window", drawn);
+                drawn = img.clone();
+            }
+            break;
+        default:
+            break;
     }
 }
 
@@ -34,38 +45,41 @@ std::vector<std::string> getFiles(const std::string& dir) {
     }
 
     files.erase(files.begin(), files.begin() + 2);
+    std::sort(files.begin(), files.end());
     return files;
 }
 
 int main(int argc, char** argv) {
-    std::string dir = "/media/vladislav/HDD/Монетки/50 копеек/";
+    std::string dir = "/media/vladislav/HDD/coins/10 рублей/";
     std::string outdir = dir + "crop/";
     system(("mkdir \"" + outdir + "\"").c_str());
     auto files = getFiles(dir);
 
-    for (const auto& f : files) {
+    cv::namedWindow("My Window", 1);
+    cv::setMouseCallback("My Window", callback, NULL);
+
+    for (const auto &f : files) {
         img = cv::imread(dir + f);
 
-        if ( img.empty() ) {
-            continue;
-        }
+        if (img.empty()) continue;
 
-        cv::resize(img, img, cv::Size(0,0), 0.3, 0.3);
+        cv::resize(img, img, cv::Size(0, 0), 0.3, 0.3);
         drawn = img.clone();
+        cv::rectangle(drawn, top, bottom, cv::Scalar(255, 0, 0), 3);
 
-        cv::namedWindow("My Window", 1);
-        cv::setMouseCallback("My Window", CallBackFunc, NULL);
         cv::imshow("My Window", drawn);
 
         while (true) {
             auto c = cv::waitKey();
 
-            if (c == 536871027) {
+            if (c == S) {
                 cv::Rect roi(top, bottom);
-                auto crop = img(roi);
-                cv::imwrite(outdir + f, crop);
-                break;
-            } else if (c == 536870944) {
+                if (roi.width > 0 && roi.height > 0) {
+                    auto crop = img(roi);
+                    cv::imwrite(outdir + f, crop);
+                    break;
+                }
+            } else if (c == SPACE) {
                 break;
             }
         }
